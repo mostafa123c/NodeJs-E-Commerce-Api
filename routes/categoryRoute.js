1ea7366cabb08b3eb043bbe9080ed3948ad455c1
mostafa123c <mostafaemad112744@gmail.com>
2023-09-18 19:26:14 +0300

    Validation Layer

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -6,14 +6,15 @@ const { getCategories,
         updateCategory,
         deleteCategory
      } = require('../services/categoryService');
+const { getcategoryValidator, createCategoryValidator, updateCategoryValidator, deleteCategoryValidator } = require('../utils/validators/categoryValidator');
 
 const router = express.Router();
 
-router.route("/").get(getCategories).post(createCategory);
+router.route("/").get(getCategories).post( createCategoryValidator, createCategory);
 router.route("/:id")
-    .get(getCategory)
-    .put(updateCategory)
-    .delete(deleteCategory);
+    .get(getcategoryValidator,getCategory)
+    .put(updateCategoryValidator,updateCategory)
+    .delete(deleteCategoryValidator,deleteCategory);
 
 
 
---
+++ utils/validators/categoryValidator.js
@@ -0,0 +1,29 @@
+const { check } = require('express-validator');
+const validatorMiddleware = require('../../middlewares/validatorMiddleware');
+
+
+exports.getcategoryValidator = [
+    check('id').isMongoId().withMessage("Invalid category Id Format"),
+    validatorMiddleware
+];
+
+exports.createCategoryValidator = [
+    check('name').notEmpty().withMessage("Category name is required")
+    .isLength({min: 3}).withMessage("Category name must be at least 3 characters long")
+    .isLength({max: 32}).withMessage("Category name must be at most 32 characters long"),
+    validatorMiddleware
+];
+
+exports.updateCategoryValidator = [
+    check('id').isMongoId().withMessage("Invalid category Id Format"),
+    check('name').notEmpty().withMessage("Category name is required")
+    .isLength({min: 3}).withMessage("Category name must be at least 3 characters long")
+    .isLength({max: 32}).withMessage("Category name must be at most 32 characters long"),
+    validatorMiddleware
+];
+
+exports.deleteCategoryValidator = [
+    check('id').isMongoId().withMessage("Invalid category Id Format"),
+    validatorMiddleware
+];
+
