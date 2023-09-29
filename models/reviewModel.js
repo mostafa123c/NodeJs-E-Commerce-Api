be1c0df8e8274f8a81f982a240621037f7834306
mostafa123c <mostafaemad112744@gmail.com>
2023-09-29 15:11:57 +0300

    Review Schema & Model

---
+++ models/reviewModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+    },
+
+    ratings: {
+      type: Number,
+      required: [true, "Rating is required"],
+      min: [1, "min rating value is 1.0"],
+      max: [5, "max rating value is 5.0"],
+    },
+
+    product: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Product",
+      required: [true, "Review must belong to a product"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Review must belong to a user"],
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("Review", reviewSchema);
