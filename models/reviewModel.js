3e6a18434f0f60b08bb4e63e59b0d4b1a7aa37ca
mostafa123c <mostafaemad112744@gmail.com>
2023-09-30 16:14:51 +0300

    Calculate Ratings Quantity And Average When Create Review

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Product = require("./productModel");
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -31,6 +32,41 @@ const reviewSchema = new mongoose.Schema(
 reviewSchema.pre(/^find/, function (next) {
   this.populate({ path: "user", select: "name" });
   next();
+});
+
+reviewSchema.statics.calcAverageRatingsAndQuantity = async function (
+  productId
+) {
+  const result = await this.aggregate([
+    // Stage 1: get all reviews on specific product
+    {
+      $match: { product: productId },
+    },
+    // Stage 2: Grouping reviews based on productID and calculate [avgRatings - ratingsQuantity]
+    {
+      $group: {
+        _id: "product",
+        avgRatings: { $avg: "$ratings" },
+        ratingsQuantity: { $sum: 1 },
+      },
+    },
+  ]);
+
+  if (result.length > 0) {
+    await Product.findByIdAndUpdate(productId, {
+      ratingsAverage: result[0].avgRatings,
+      ratingsQuantity: result[0].ratingsQuantity,
+    });
+  } else {
+    await Product.findByIdAndUpdate(productId, {
+      ratingsAverage: 0,
+      ratingsQuantity: 0,
+    });
+  }
+};
+
+reviewSchema.post("save", async function () {
+  await this.constructor.calcAverageRatingsAndQuantity(this.product);
 });
 
 module.exports = mongoose.model("Review", reviewSchema);
