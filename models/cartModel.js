acbba6ab75836553cded02427de7eac377cee0c1
mostafa123c <mostafaemad112744@gmail.com>
2023-10-01 16:23:57 +0300

    Shopping Cart Schema & Model

---
+++ models/cartModel.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const cartSchema = new mongoose.Schema(
+  {
+    cartItems: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+        price: Number,
+        color: String,
+      },
+    ],
+    totalCartPrice: Number,
+    totalPriceAfterDiscount: Number,
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Cart", cartSchema);
