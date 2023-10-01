6ed1e7d120ba9fa50c13c861404fe100f72dc7a4
mostafa123c <mostafaemad112744@gmail.com>
2023-10-01 19:05:23 +0300

    Get Logged User Shopping Cart

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -1,11 +1,14 @@
 const express = require("express");
 
-const { addProductToCart } = require("../services/cartService");
+const {
+  addProductToCart,
+  getLoggedUserCart,
+} = require("../services/cartService");
 const authService = require("../services/authService");
 
 const router = express.Router();
 
 router.use(authService.protect, authService.allowedTo("user"));
-router.route("/").post(addProductToCart);
+router.route("/").post(addProductToCart).get(getLoggedUserCart);
 
 module.exports = router;
--- services/cartService.js
+++ services/cartService.js
@@ -58,3 +58,22 @@ exports.addProductToCart = asyncHandler(async (req, res, next) => {
     data: cart,
   });
 });
+
+// @desc    Get logged user cart
+// @route   GET /api/v1/cart
+// @access  Private/User
+exports.getLoggedUserCart = asyncHandler(async (req, res, next) => {
+  const cart = await Cart.findOne({ user: req.user._id });
+
+  if (!cart) {
+    return next(
+      new ApiError(`There is no cart for this user id : ${req.user._id}`, 404)
+    );
+  }
+
+  res.status(200).json({
+    status: "success",
+    numOfCartItems: cart.cartItems.length,
+    data: cart,
+  });
+});
