ec6dcd473b75e0260a905e0044927746368ba6ec
mostafa123c <mostafaemad112744@gmail.com>
2023-10-01 13:30:13 +0300

    Refactor Mount Routes Middleware

--- server.js
+++ server.js
@@ -9,21 +9,12 @@ const ApiError = require("./utils/apiError");
 const globalError = require("./middlewares/errorMiddleware");
 const dbConnection = require("./config/database");
 // Routes
-const categoryRoute = require("./routes/categoryRoute");
-const subCategoryRoute = require("./routes/subCategoryRoute");
-const brandRoute = require("./routes/brandRoute");
-const productRoute = require("./routes/productRoute");
-const userRoute = require("./routes/userRoute");
-const authRoute = require("./routes/authRoute");
-const reviewRoute = require("./routes/reviewRoute");
-const wishlistRoute = require("./routes/wishlistRoute");
-const addressRoute = require("./routes/addressRoute");
-const couponRoute = require("./routes/couponRoute");
+const mountRoutes = require("./routes");
 
 // Connect to DB
 dbConnection();
 
-//express app
+//express ap
 const app = express();
 
 // Middlewares
@@ -36,16 +27,7 @@ if (process.env.NODE_ENV === "development") {
 }
 
 // Mount Routes
-app.use("/api/v1/categories", categoryRoute);
-app.use("/api/v1/subcategories", subCategoryRoute);
-app.use("/api/v1/brands", brandRoute);
-app.use("/api/v1/products", productRoute);
-app.use("/api/v1/users", userRoute);
-app.use("/api/v1/auth", authRoute);
-app.use("/api/v1/reviews", reviewRoute);
-app.use("/api/v1/wishlist", wishlistRoute);
-app.use("/api/v1/addresses", addressRoute);
-app.use("/api/v1/coupons", couponRoute);
+mountRoutes(app);
 
 app.all("*", (req, res, next) => {
   // create error and send it to error handling middleware
