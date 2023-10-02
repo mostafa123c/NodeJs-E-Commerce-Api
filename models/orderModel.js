fab59c63e749b9266a40eef3c5ef7a26c8999f7c
mostafa123c <mostafaemad112744@gmail.com>
2023-10-02 10:03:49 +0300

    Order Schema & Model

---
+++ models/orderModel.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+const orderSchrma = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Order Must Be belong To User"],
+    },
+
+    cartItems: [
+      {
+        product: {
+          type: mongoose.Schema.ObjectId,
+          ref: "Product",
+        },
+        quantity: Number,
+        price: Number,
+        color: String,
+      },
+    ],
+
+    taxPrice: {
+      type: Number,
+      default: 0,
+    },
+
+    shippingPrice: {
+      type: Number,
+      default: 0,
+    },
+
+    totalOrderPrice: Number,
+
+    paymentMethodType: {
+      type: String,
+      enum: ["card", "cash"],
+      default: "cash",
+    },
+
+    isPaid: {
+      type: Boolean,
+      default: false,
+    },
+
+    paidAt: Date,
+
+    isDelivered: {
+      type: Boolean,
+      default: false,
+    },
+
+    deliveredAt: Date,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Order", orderSchrma);
